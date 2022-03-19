c706d4d87e4e0a53b0c063fa3dd36d2599d28d01
gabriel reis <gabriel.nogueira@assessoriablue.com.br>
2022-03-19 00:56:25 -0300

    ajustando json de entrada e saida

--- triangulation.py
+++ triangulation.py
@@ -24,16 +24,23 @@ def connecting(p):
 
 def create_sala_blue():
     plt.plot(x_list, y_list, 'o')
+
     connecting(pontos_cafe)
     connecting(pontos_sala_reuniao)
     connecting(pontos_sala_socios)
     connecting(pontos_copa)
     connecting(pontos_area_trabalho)
+    
     plt.plot(beacon_1.get_x(), beacon_1.get_y(), 'ro')
+    plt.text(beacon_1.get_x()+0.2, beacon_1.get_y(), '1')
     plt.plot(beacon_2.get_x(), beacon_2.get_y(), 'ro')
+    plt.text(beacon_2.get_x()-0.5, beacon_2.get_y(), '2')
     plt.plot(beacon_3.get_x(), beacon_3.get_y(), 'ro')
+    plt.text(beacon_3.get_x()+0.2, beacon_3.get_y(), '3')
     plt.plot(beacon_4.get_x(), beacon_4.get_y(), 'ro')
+    plt.text(beacon_4.get_x()+0.2, beacon_4.get_y(), '4')
     plt.plot(beacon_5.get_x(), beacon_5.get_y(), 'ro')
+    plt.text(beacon_5.get_x()-0.5, beacon_5.get_y(), '5')
     
     return
 
@@ -96,17 +103,15 @@ def rooturl():
 def locate():
     posted_beacons = request.json['beacons']
     precision = 5
-    nearests_beacon = [
-        (beacons[posted_beacons[0]["id"]].point, round(posted_beacons[0]["distance"], precision)),
-        (beacons[posted_beacons[1]["id"]].point, round(posted_beacons[1]["distance"], precision)),
-        (beacons[posted_beacons[2]["id"]].point, round(posted_beacons[2]["distance"], precision)),
-    ]
-    sorted_distances = sorted(nearests_beacon, key=lambda x : x[1])
+    nearests_beacons = [(beacons[posted_beacons[i]["id"]].point, round(posted_beacons[i]["distance"], precision)) for i in range(len(posted_beacons))]
+    sorted_distances = sorted(nearests_beacons, key=lambda x : x[1])
 
     p1, r1 = sorted_distances[0]
     p2, r2 = sorted_distances[1]
     p3, r3 = sorted_distances[2]
-    
+    for i in sorted_distances:
+        print(i)
+
     circle_p1 = Circle(p1[0], p1[1], r1)
     circle_p2 = Circle(p2[0], p2[1], r2)
     resultado, p4 = find_fourth_coordinate(p3, circle_p1.circle_intersect(circle_p2), r3) 
@@ -117,14 +122,28 @@ def locate():
     try: 
         for polygon_name, polygon in polygons.items():
             if polygon.contains(point_n):
-                print(f'ponto dentro de {polygon_name}')
                 create_sala_blue()    
                 plt.plot(point_n.x, point_n.y, 'g*')
                 plt.savefig('test_fig.png')
                 plt.close()
         
-                return {'Area': polygon_name}
-        return {'Area': "nenhuma"}
+                return {
+                    'Area': polygon_name,
+                    'x': point_n.x,
+                    'y': point_n.y,
+                    'Erro': None
+                }
+        return {
+            'Area': None,
+            'x': point_n.x,
+            'y': point_n.y,
+            'Erro': None
+        }
     
     except Exception as e:
-        return {'Erro': e.args()}
+        return {
+            'Area': None,
+            'x': None,
+            'y': None,
+            'Erro': e.args
+        }
