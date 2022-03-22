1d03742d6acd1ab14014ef2e9af5154fec007e68
gabriel reis <gabriel.nogueira@assessoriablue.com.br>
2022-03-22 20:57:29 -0300

    preparando python para analise da planta

--- triangulation.py
+++ triangulation.py
@@ -6,28 +6,36 @@ from area.area import Area
 from beacon.beacons import Beacons
 from circle.circle import Circle
 
-area_pontos = [
-    [0,0], [6.7, 0], [10.05, 0], [13.4, 0], 
-    [0, 3.3], [2.75, 3.3], [6.7, 3.3], [10.05, 3.3], [13.4, 3.3],
-    [0, 5.2], [2.75, 5.2], [6.7, 5.2], [13.4, 5.2]
-]
+def teste_blue():
+    area_pontos = [
+        [0,0], [6.7, 0], [10.05, 0], [13.4, 0], 
+        [0, 3.3], [2.75, 3.3], [6.7, 3.3], [10.05, 3.3], [13.4, 3.3],
+        [0, 5.2], [2.75, 5.2], [6.7, 5.2], [13.4, 5.2]
+    ]
 
-area = Area(area_pontos)
-area.Criar_Poligono('cafe', [[6.7, 3.3], [6.7, 5.2], [13.4, 5.2], [13.4, 3.3]])
-area.Criar_Poligono('sala_reuniao', [[10.05, 0], [10.05, 3.3], [13.4, 3.3], [13.4, 0]])
-area.Criar_Poligono('sala_socios', [[6.7, 0], [6.7, 3.3], [10.05, 3.3], [10.05, 0]])
-area.Criar_Poligono('copa', [[0, 3.3], [0, 5.2], [2.75, 5.2], [2.75, 3.3]])
-area.Criar_Poligono('area_trabalho', [[0, 0], [6.7, 0], [6.7, 3.3], [6.7, 5.2], [2.75, 5.2], [2.75, 3.3], [0, 3.3]])
+    area = Area(area_pontos)
+    area.Criar_Poligono('cafe', [[6.7, 3.3], [6.7, 5.2], [13.4, 5.2], [13.4, 3.3]])
+    area.Criar_Poligono('sala_reuniao', [[10.05, 0], [10.05, 3.3], [13.4, 3.3], [13.4, 0]])
+    area.Criar_Poligono('sala_socios', [[6.7, 0], [6.7, 3.3], [10.05, 3.3], [10.05, 0]])
+    area.Criar_Poligono('copa', [[0, 3.3], [0, 5.2], [2.75, 5.2], [2.75, 3.3]])
+    area.Criar_Poligono('area_trabalho', [[0, 0], [6.7, 0], [6.7, 3.3], [6.7, 5.2], [2.75, 5.2], [2.75, 3.3], [0, 3.3]])
 
-beacons = Beacons()
-beacons.criar(1, 8.375, 3.3)
-beacons.criar(2, 13.4, 4.5)
-beacons.criar(3, 2.75, 4.5)
-beacons.criar(4, 0, 1.5)
-beacons.criar(5, 10.05, 1.5)
+    beacons = Beacons()
+    beacons.criar(1, 8.375, 3.3)
+    beacons.criar(2, 13.4, 4.5)
+    beacons.criar(3, 2.75, 4.5)
+    beacons.criar(4, 0, 1.5)
+    beacons.criar(5, 10.05, 1.5)
 
-app = Flask(__name__)
-CORS(app)
+    return area, beacons
+area, beacons = teste_blue()
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    return app
+app = create_app()
 
 @app.route('/')
 def rooturl():
@@ -42,13 +50,13 @@ def locate():
     nearests_beacons = [(beacons.get_beacon(posted_beacons[i]["id"]).point, round(posted_beacons[i]["distance"], precision)) for i in range(len(posted_beacons))]  
     sorted_distances = sorted(nearests_beacons, key=lambda x : x[1])
 
-    p1, r1 = sorted_distances[0]
-    p2, r2 = sorted_distances[1]
-    p3, r3 = sorted_distances[2]
+    ponto_1, raio_1 = sorted_distances[0]
+    ponto_2, raio_2 = sorted_distances[1]
+    ponto_3, raio_3 = sorted_distances[2]
 
-    circle_p1 = Circle(p1[0], p1[1], r1)
-    circle_p2 = Circle(p2[0], p2[1], r2)
-    resultado, p4 = area.achar_coordenada(p3, circle_p1.circle_intersect(circle_p2), r3) 
+    circle_p1 = Circle(ponto_1[0], ponto_1[1], raio_1)
+    circle_ponto_2 = Circle(ponto_2[0], ponto_2[1], raio_2)
+    resultado, p4 = area.achar_coordenada(ponto_3, circle_p1.circle_intersect(circle_ponto_2), raio_3) 
     if not resultado:
         return {'Erro': p4} 
 
@@ -79,3 +87,9 @@ def locate():
     except Exception as e:
         retorno['Erro'] = e.args
         return retorno
+
+
+if __name__ == "__main__":
+    teste_blue()
+    area, beacons = teste_blue()
+    app.run()
